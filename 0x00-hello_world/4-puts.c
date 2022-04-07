f201f330943f3b85397c72add29f2dd6cf766aeb
Biruk-G3 <byafet3@gmail.com>
2022-04-07 01:56:32 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* the first line to be inturpreated is the main function
+ * main - entry point
+ * return0: after printing
+ * */
+int main(void){
+puts ("\"Programming is like building a multilingual puzzle");
+return 0;
+}
+
