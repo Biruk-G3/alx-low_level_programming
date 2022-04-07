9510518aa29cbf1afb93ae3ffecd1429895d46ca
Biruk-G3 <byafet3@gmail.com>
2022-04-07 10:55:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return: return 1 after the function
+ */
+int main(void)
+{
+char ch;
+ch="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+putchar(ch);
+return 1;
+}
