e80517fe3d69f440ed25fcd775748142aed272b9
Biruk-G3 <byafet3@gmail.com>
2022-04-08 11:01:25 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
 {
-printf("%d is postive\n",n);
+printf("%d is positive\n",n);
 }
 else if( n == 0)
 {
-printf("%d is equll zero\n",n);
+printf("%d is zero\n",n);
 }
 else
 {
