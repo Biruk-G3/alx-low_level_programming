b42013d2f34283d2e8fdbd11e9cf77861549cd56
Biruk-G3 <byafet3@gmail.com>
2022-04-08 04:08:09 -0700

    postive egative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n>0){
+	pritf("is postive");
+	}
+else if( n == 0){
+	printf("is equll zero");
+
+}
+else{
+	pritf("is negative");
+}
+
+return (0);
+
+}
