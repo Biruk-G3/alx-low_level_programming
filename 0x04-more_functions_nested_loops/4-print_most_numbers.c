a4aa5df1c2d1e191c02111a01d37413cab5955f0
Biruk-G3 <byafet3@gmail.com>
2022-04-14 10:13:36 -0700

    while

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	int num;
+	int num = 0;
 
-	for (num = 0; num <= 9; num++)
+	while (num <= 9)
 	{
 		if (num != 2 && num != 4)
+			continue;
+		else
 			_putchar((num % 10) + '0');
 	}
 
