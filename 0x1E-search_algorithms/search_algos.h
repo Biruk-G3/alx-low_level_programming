d5f14a9630e7ec61755dab6657959a172bd5f024
BirukG <byafet3@gmail.com>
2022-10-31 09:06:04 -0700

    search algorithms binary and linnar

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,20 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes required for
+ * the 0x1E-search_algorithms project.
+ *
+ * Author: Biruk Gelelcha
+ * Date Created: Oct 31, 2022
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
